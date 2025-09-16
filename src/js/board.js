6fef4e77649864d83efbdc8321475016a1132588
unknown <zz240614@gmail.com>
2025-09-16 22:45:16 +0100

    created function createBoard user can chose number of cards of board

---
+++ src/js/board.js
@@ -0,0 +1,27 @@
+function createBoard() {
+  const board = document.querySelector(".board");
+  const numberOfCardInput = document.getElementById("numberOfCard");
+  
+  let number = numberOfCardInput.value;
+  if (!number || number < 3) {
+    alert("Please enter a number");
+    return;
+  }
+
+  board.innerHTML = "";
+  
+  let sumOfNumber = number * number;
+  for (let i = 0; i < sumOfNumber; i++) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerText = "";
+    board.appendChild(card);
+  }
+
+  board.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+  board.style.gap = "2px";
+  board.style.width = "500px";
+  board.style.height = "500px";
+  board.style.display = "grid";
+}
---
+++ src/js/play.js
@@ -0,0 +1,12 @@
+function play ()
+{
+    const players = ["X", "O"];
+    const cards = document.querySelectorAll(".card");
+    let player_1 ; 
+    let player_2 ;
+
+    if ( !player_1 && !player_2 ){
+        player_1 onclick  }
+
+
+}
